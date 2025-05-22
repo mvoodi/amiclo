09a3ebd7aacf196e2646313438f08e18175f9a59
Aleksandra Arykova <aleksandra.arykova@alatoo.edu.kg>
2025-05-22 19:49:26 +0600

    initial commit

---
+++ src/main/java/com/example/amiclo/AmicloApplication.java
@@ -0,0 +1,13 @@
+package com.example.amiclo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmicloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmicloApplication.class, args);
+	}
+
+}
