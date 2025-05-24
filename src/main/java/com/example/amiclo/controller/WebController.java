0b1fe99664cff63683522af2bac42c2f1c899759
Aleksandra Arykova <aleksandra.arykova@alatoo.edu.kg>
2025-05-24 12:34:48 +0600

    add LoginController and WebController

---
+++ src/main/java/com/example/amiclo/controller/WebController.java
@@ -0,0 +1,41 @@
+package com.example.amiclo.controller;
+
+import com.example.amiclo.model.Request;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("pageTitle", "Главная — AmiClo");
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("pageTitle", "О нас — AmiClo");
+        return "about";
+    }
+
+    @GetMapping("/services")
+    public String services(Model model) {
+        model.addAttribute("pageTitle", "Услуги — AmiClo");
+        return "services";
+    }
+
+    @GetMapping("/contacts")
+    public String contacts(Model model) {
+        model.addAttribute("pageTitle", "Контакты — AmiClo");
+        return "contacts";
+    }
+
+    // Чтобы форма заявки в contacts.html не выбрасывала ошибку
+    @ModelAttribute("request")
+    public Request request() {
+        return new Request();
+    }
+}
